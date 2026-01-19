57c54e6af7f71578a2cf1729528638b86444996e
whoisyashu <maheshwariy077@gmail.com>
2026-01-19 15:41:56 +0530

    Added an api for specific student by id

--- Day2Task2/src/main/java/com/example/day2task2/controller/Home.java
+++ Day2Task2/src/main/java/com/example/day2task2/controller/Home.java
@@ -2,14 +2,16 @@ package com.example.day2task2.controller;
 
 import com.example.day2task2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @RestController
 public class Home {
+
     @GetMapping("/")
-    public ArrayList<StudentModel> getStudent(){
+    public ArrayList<StudentModel> getStudent() {
         ArrayList<StudentModel> students = new ArrayList<>();
         students.add(new StudentModel(1, "Yash", "maheshwariy077@gmail.com"));
         students.add(new StudentModel(2, "Yashasvi", "maheshwariy076@gmail.com"));
@@ -17,5 +19,17 @@ public class Home {
         students.add(new StudentModel(4, "Yogendra", "maheshwariy074@gmail.com"));
         students.add(new StudentModel(5, "Yuvraj", "maheshwariy073@gmail.com"));
         return students;
+    }
+
+    @GetMapping("/student/{id}")
+    public StudentModel getStudentById(@PathVariable int id) {
+        ArrayList<StudentModel> students = getStudent();
+
+        for (StudentModel student : students) {
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+        return null;
     }
 }
